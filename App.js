4469c6ec8469b2c5ea6fcb40049d6cc1f0e5184d
Sunn1024 <sunnysharma12892@gmail.com>
2026-03-20 17:01:00 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
-  return (
+  return ( 
     <View style={styles.container}>
       <Text style={styles.text}>Othello Game</Text>
       <Text style={styles.sub}>Build test successful!</Text>
